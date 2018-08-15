f0a2ec266eb14efaee3de22a9ad5fa422d3e0115
Angel Rafael Acosta Cardozo <angel.rac777@gmail.com>
2018-08-14 22:03:42 -0400

    Tu hermana suma listas y falta elegir el mayor

--- tp2.sh
+++ tp2.sh
@@ -5,8 +5,12 @@ mvn clean install package
 start-dfs.sh
 start-yarn.sh
 
+#Borra los datos y la salida del hdfs para que no cree conflicto si ya existe
+hdfs dfs -rm /datos
+hdfs dfs -rm -r /salida
+
 #Copia el archivo con los datos al hdfs
-hdfs dfs -copyFromLocal datos/datos.txt /datos
+hdfs dfs -copyFromLocal datos/datos.csv /datos
 
 #Ejecuta el programa
 hadoop jar target/tp2-1.0-SNAPSHOT.jar  /datos /salida
@@ -14,6 +18,3 @@ hadoop jar target/tp2-1.0-SNAPSHOT.jar  /datos /salida
 #Muestra la salida del programa
 hdfs dfs -cat /salida/*
 
-#Borra los datos y la salida del hdfs
-hdfs dfs -rm /datos
-hdfs dfs -rm -r /salida
